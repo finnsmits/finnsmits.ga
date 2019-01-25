db35303e17fa64ad991b0562f348ecf1e8956f99
Finn Smits <finn.revy@outlook.com>
2019-01-25 14:14:09 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,5 +2,5 @@ var knop = document.querySelector('#todoadd');
 var invoerveld = document.querySelector('#taakv');
 
 knop.onclick = function() {
-    alert('waarde: '+invoerveld.value)
+    alert('nieuwe taak: '+invoerveld.value)
 }
