e01bad706276b4534ae44407be4d5b5ec894ccdc
Finn Smits <finn.revy@outlook.com>
2019-01-25 13:39:50 +0100

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,6 +1,3 @@
-var mijnknop = document.querySelector('todoadd')
-function todoadd {
-    var to do list = prompt('voer taak in');
-    localStorage.setItem('voer taak in, todoadd');
-    mijnhoofding.textcontent = '' + todoadd
+todoadd.unclick = function() {
+    voer taak in();
 }
